8e7c0c71f573c4940fff6e57ef9cea01759ba036
jsbsjv <123757167+jsbsjv@users.noreply.github.com>
2023-09-11 14:55:53 +0300

    Add files via upload

---
+++ jepthon/plugins/متحركات كيوت.py
@@ -0,0 +1,141 @@
+#edit  ~ @SC_60 for jepiq 
+
+from telethon import events
+from jepthon.utils import admin_cmd
+from jepthon import jepiq
+from . import *
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+
+plugin_category = "extra"
+@jepiq.ar_cmd(
+    pattern="ك1$",
+    command=("ك1", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    ROZ = await reply_id(event)
+    if cute:
+        jepiq = f"**{ROZA}**\n"
+        jepiq += f"✛━━━━━━━━━━━━━✛\n"
+        jepiq += f"**الـمتحـرڪـة الأولـى **"
+        await event.client.send_file(event.chat_id, cute, caption=jepiq, reply_to=ROZ)
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك2$",
+    command=("ك2", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    leo = await reply_id(event)
+    if cute2:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـانيـة **\n"
+        await event.client.send_file(event.chat_id, cute2, caption=RAZAN, reply_to=leo)
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك3$",
+    command=("ك3", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    sic_id = await reply_id(event)
+    if cute3:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـثـالثـة**\n"
+        await event.client.send_file(event.chat_id, cute3, caption=RAZAN, reply_to=sic_id)
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك4$",
+    command=("ك4", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if cute4:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـرابـعـة**\n"
+        await event.client.send_file(
+            event.chat_id, cute4, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك5$",
+    command=("ك5", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if cute5:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـخامسـة**\n"
+        await event.client.send_file(
+            event.chat_id, cute5, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒جوكر تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك6$",
+    command=("ك6", plugin_category),
+           )
+
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if cute6:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسادسـة**\n"
+        await event.client.send_file(
+            event.chat_id, cute6, caption=RAZAN, reply_to=reply_to_id
+        )
+
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك
+
+@jepiq.ar_cmd(
+    pattern="ك7$",
+    command=("ك7", plugin_category),
+           )
+async def tmgif(event):
+    if event.fwd_from:
+        return
+    reply_to_id = await reply_id(event)
+    if cute7:
+        RAZAN = f"**{ROZA}**\n"
+        RAZAN += f"✛━━━━━━━━━━━━━✛\n"
+        RAZAN += f"**الـمتحـرڪـة الـسـابعـة**"
+        await event.client.send_file(
+            event.chat_id, cute7, caption=RAZAN, reply_to=reply_to_id
+        )
+    
+       
+#edit  ~ @SC_60 for jepiq 
+#جميع الحقوق محفوظة لسـورس 𝑠𝑜𝑢𝑛𝑑𝑤𝑎𝑣𝑒 تخـمط تبيـن فشلـك   
